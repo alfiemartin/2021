6ff1df2d111c2dc017c119462eed414ba026231d
Alfie <alfie.martin@remarkable.net>
2021-09-26 14:17:39 +0100

    accordion updates

--- components/accordion/Accordion.tsx
+++ components/accordion/Accordion.tsx
@@ -2,7 +2,7 @@ import React, { ReactElement, useEffect, useRef, useState } from 'react'
 import { AccordionProps, AccordionContext, AccordionItemProps, useAccordionContext, getStyling } from './accordionContext';
 
 const Accordion = ({children, styling="none"}: AccordionProps) => {
-    const [activeItems, setActiveItems] = useState<number[]>([]);
+    const [openItems, setOpenItems] = useState<number[]>([]);
 
     const wrapperRefs = useRef<HTMLDivElement[]>([]);
     const contentRefs = useRef<HTMLDivElement[]>([]);
@@ -11,20 +11,20 @@ const Accordion = ({children, styling="none"}: AccordionProps) => {
     
     useEffect(() => {
         wrapperRefs.current.forEach((wrapperRef, i) => {
-            if(activeItems.includes(i)) {
+            if(openItems.includes(i)) {
                 wrapperRef.style.maxHeight = `${contentRefs.current[i].clientHeight}px`;
             } else {
                 wrapperRef.style.maxHeight = `0px`;
             }
         });
 
-    }, [activeItems])
+    }, [openItems])
 
     return (
-        <AccordionContext.Provider value={{setActiveItems, activeItems, wrapperRefs, contentRefs, styles}}>
+        <AccordionContext.Provider value={{setOpenItems, openItems, wrapperRefs, contentRefs, styles}}>
             <div className={`Accordion border-2 border-black ${styles.accordionStyles}`}>
-                {React.Children.map(children, (child, count = 0) => {
-                    return React.cloneElement(child as ReactElement, {id: count++})
+                {React.Children.map(children, (accordionItem, count = 0) => {
+                    return React.cloneElement(accordionItem as ReactElement, {id: count++})
                 })}
             </div>
         </AccordionContext.Provider>
@@ -35,26 +35,25 @@ const AccordionItem = ({children, id}: AccordionItemProps) => {
     
     return (
         <div className={`AccordionItem`}>
-            {React.Children.map(children, child => {
-                return React.cloneElement(child as ReactElement, {id})
+            {React.Children.map(children, accordionItemChildren => {
+                return React.cloneElement(accordionItemChildren as ReactElement, {id})
             })}
         </div>
     )
 } 
 
 const AccordionItemTitle = ({children, id}: AccordionItemProps) => {
-    const {setActiveItems, activeItems, styles} = useAccordionContext();
+    const {setOpenItems, openItems, styles} = useAccordionContext();
     
-    const itemIsActive = (id:number) => {
-        return activeItems.indexOf(id) !== -1;
+    const itemIsActive = (id: number) => {
+        return openItems.indexOf(id) !== -1;
     }
 
     const updateActiveItems = () => {
-        
         if(itemIsActive(id)) { 
-            setActiveItems(currentActiveItems => currentActiveItems.filter((value) => value !== id));
+            setOpenItems(currentActiveItems => currentActiveItems.filter((value) => value !== id));
         } else {
-            setActiveItems(currentActiveItems => [...currentActiveItems, id])
+            setOpenItems(currentActiveItems => [...currentActiveItems, id])
         }
     }
 
