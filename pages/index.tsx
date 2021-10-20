fbfa191b99f797bb6b13fd231f9544af5b96bed0
alfie <alfie.martin@remarkable.net>
2021-10-20 17:16:16 +0100

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,11 @@ import LayoutContained from "../components/layouts/LayoutContained";
 import Image from "next/image";
 
 import image from "../public/image2.jpg";
-import Accordion, {AccordionItem, AccordionItemContent, AccordionItemTitle} from "../components/accordion/Accordion";
+import Accordion, {
+  AccordionItem,
+  AccordionItemContent,
+  AccordionItemTitle,
+} from "../components/Accordion/Accordion";
 
 export default function Home() {
   return (
@@ -11,35 +15,28 @@ export default function Home() {
       <div className="w-1/3 mt-8">
         <Accordion styling="none">
           <AccordionItem>
-            <AccordionItemTitle>
-              Hello
-            </AccordionItemTitle>
+            <AccordionItemTitle>Hello</AccordionItemTitle>
             <AccordionItemContent>
               <Image src={image} layout="responsive" />
             </AccordionItemContent>
           </AccordionItem>
         </Accordion>
       </div>
+
       <div className="w-1/3 mt-8">
-      <Accordion styling="none">
-        <AccordionItem>
-          <AccordionItemTitle>
-            Hello
-          </AccordionItemTitle>
-          <AccordionItemContent>
-            <Image src={image} layout="responsive" />
-          </AccordionItemContent>
-        </AccordionItem>
-      </Accordion>
-    </div>
+        <Accordion styling="none">
+          <AccordionItem>
+            <AccordionItemTitle>Hello</AccordionItemTitle>
+            <AccordionItemContent>
+              <Image src={image} layout="responsive" />
+            </AccordionItemContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
     </div>
   );
 }
 
 Home.getLayout = (page: ReactElement) => {
-  return (
-    <LayoutContained>
-      {page}
-    </LayoutContained>
-  )
-}
+  return <LayoutContained>{page}</LayoutContained>;
+};
